6fe80cdae0a5cf25e05bdf7e4d189779e1424e1d
Benjamin Pašić <bpasic1@etf.unsa.ba>
2023-05-10 15:49:25 +0200

    Urađen SpringCloudGateway (PZ7,Z1)

---
+++ SpringCloudGateway/src/main/java/ba/tim2/SpringCloudGateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package ba.tim2.SpringCloudGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayApplication.class, args);
+	}
+
+}
---
+++ SpringCloudGateway/src/test/java/ba/tim2/SpringCloudGateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.tim2.SpringCloudGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
