1a7bd6e1cae0c2409ea8d5687e27c21f3a8d031d
Benjamin Pašić <bpasic1@etf.unsa.ba>
2023-05-17 00:07:59 +0200

    Uradjena centralizacija.

---
+++ ConfigService/src/main/java/ba/tim2/configservice/ConfigServiceApplication.java
@@ -0,0 +1,16 @@
+package ba.tim2.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+
+
+}
