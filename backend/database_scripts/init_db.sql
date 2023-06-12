8c2532fda71184716768907921d2fc637fc332cc
Benjamin Pašić <bpasic1@etf.unsa.ba>
2023-06-12 03:14:21 +0200

    Docker-compose final

--- backend/database_scripts/init_db.sql
+++ backend/database_scripts/init_db.sql
@@ -3,9 +3,11 @@ GRANT ALL PRIVILEGES ON rezervacijaKarata.* TO 'batim2'@'%';
 GRANT ALL PRIVILEGES ON events.* TO 'batim2'@'%';
 GRANT ALL PRIVILEGES ON preporucivanje_sadrzaja_pogodnosti.* TO 'batim2'@'%';
 GRANT ALL PRIVILEGES ON auth_service.* TO 'batim2'@'%';
+GRANT ALL PRIVILEGES ON upravljanje_filmovima_kartama.* TO 'batim2'@'%';
 FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS rezervacijaKarata;
 CREATE DATABASE IF NOT EXISTS events;
 CREATE DATABASE IF NOT EXISTS preporucivanje_sadrzaja_pogodnosti;
 CREATE DATABASE IF NOT EXISTS auth_service;
+CREATE DATABASE IF NOT EXISTS upravljanje_filmovima_kartama;
