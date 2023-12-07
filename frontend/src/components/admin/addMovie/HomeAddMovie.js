2c022d3aa88749a70ae15073c425c6430af7bb06
Benjamin Pašić <bpasic1@etf.unsa.ba>
2023-12-07 16:46:20 +0100

    Adjusted routing for admin routes /addUser and /addMovies

--- frontend/src/components/admin/addMovie/HomeAddMovie.js
+++ frontend/src/components/admin/addMovie/HomeAddMovie.js
@@ -1,13 +1,56 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import AppbarAdmin from "../AppbarAdmin";
 import AddMovie from "./AddMovie.js";
 
+
+import jwt_decode from 'jwt-decode';
+
+
 function HomeAddMovie() {
-  return (
-    <>
-      <AppbarAdmin />
-      <AddMovie />
-    </>
+    const [isLogged, setIsLogged] = useState(true); // Track whether the user is logged in
+  const [isAdmin, setIsAdmin] = useState(false); // Track whether the user has admin role
+
+  useEffect(() => {
+    const checkLoginStatus = () => {
+      const token = localStorage.getItem("access_token");
+
+      // Check if the token is empty
+      if (!token) {
+        setIsLogged(false);
+        return;
+      }
+
+      const decodedToken = jwt_decode(token);
+
+      // Check if the user has the admin role
+      setIsAdmin(decodedToken.role === 'ROLE_ADMIN');
+
+      setIsLogged(true);
+    };
+
+    checkLoginStatus();
+  }, []);
+
+
+  return ( <>
+    {isLogged && (isAdmin && <AppbarAdmin />)}
+    {isLogged ? (
+      isAdmin ? (
+        <AddMovie />
+      ) : (
+        <div style={{ textAlign: "center", margin: "20px" }}>
+          <p>Nemate pristup. Kontaktirajte administratora.</p>
+          <button  onClick={() => window.location.href = "/homeUser"}>Return to Homepage</button>
+        </div>
+      )
+    ) : (
+      <div style={{ textAlign: "center", margin: "20px" }}>
+        <p>Morate biti prijavljeni da biste pristupili ovom sadržaju.</p>
+        {/* Add a link or button to navigate to the login page */}
+        <button onClick={() => window.location.href = "/login"}>Go to Login</button>
+      </div>
+    )}
+  </>
   );
 }
 
