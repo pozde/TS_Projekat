8a42bc0ab626c6b8be2ad43759daaf34ab0847c8
Benjamin Pašić <bpasic1@etf.unsa.ba>
2023-12-07 14:25:51 +0100

    Added logic to not show admin route /userOverview for not logged user and user without admin role

--- frontend/src/components/admin/overviewUser/HomeOverviewUser.js
+++ frontend/src/components/admin/overviewUser/HomeOverviewUser.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppbarAdmin from "../AppbarAdmin";
 import OverviewUser from "./OverviewUser";
+import jwt_decode from 'jwt-decode';
 
 function HomeOverviewUser() {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isLogged, setIsLogged] = useState(true); // Track whether the user is logged in
+
+  useEffect(() => {
+    const checkAdminRole = () => {
+      const token = localStorage.getItem("access_token");
+
+      // Check if the token is empty
+      if (!token) {
+        setIsLogged(false);
+        return;
+      }
+
+      const decodedToken = jwt_decode(token);
+      setIsAdmin(decodedToken.role === 'ROLE_ADMIN');
+    };
+
+    checkAdminRole();
+  }, []);
+
+  const returnToHomepage = () => {
+    window.location.href = "/";
+  };
+
   return (
     <>
-      <AppbarAdmin />
-      <OverviewUser />
+      {isLogged && (isAdmin && <AppbarAdmin />)}
+      {isLogged ? (
+        isAdmin ? (
+          <OverviewUser />
+        ) : (
+          <div style={{ textAlign: "center", margin: "20px" }}>
+            <p>Nemate pristup. Kontaktirajte administratora.</p>
+            <button onClick={returnToHomepage}>Return to Homepage</button>
+          </div>
+        )
+      ) : (
+        <div style={{ textAlign: "center", margin: "20px" }}>
+          <p>Morate biti prijavljeni da biste pristupili ovom sadržaju.</p>
+          {/* Add a link or button to navigate to the login page */}
+          <button onClick={() => window.location.href = "/login"}>Go to Login</button>
+        </div>
+      )}
     </>
   );
 }
--- frontend/src/components/admin/overviewUser/OverviewUser.js
+++ frontend/src/components/admin/overviewUser/OverviewUser.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Container, Paper, Button } from "@mui/material";
 import axios from "axios";
+import jwt_decode from 'jwt-decode';
 
 export default function OverviewUser() {
   const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
   const [korisnici, setKorisnici] = useState([]);
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     const fetchKorisnici = async () => {
       const token = localStorage.getItem("access_token");
+      const decodedToken = jwt_decode(token);
+
+      setIsAdmin(decodedToken.role === 'ROLE_ADMIN');
+
       try {
         const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:8081";
         const response = await axios.get(`${BASE_URL}/korisnici`, {
@@ -44,25 +50,31 @@ export default function OverviewUser() {
 
   return (
     <Container>
-      <><h1 style={{ color: "white" }}>Korisnici</h1></>
-      <Paper elevation={3} style={paperStyle}>
-        {korisnici.map((korisnik) => (
-          <>
-            <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left", color: "black" }} key={korisnik.id}>
-              Id: {korisnik.id} <br />
-              Ime: {korisnik.ime} <br />
-              Prezime: {korisnik.prezime} <br />
-              Datum rođenja: {korisnik.datumRodjenja} <br />
-              Email: {korisnik.email} <br />
-              Broj telefona: {korisnik.brojTelefona} <br />
-              Spol: {korisnik.spol}
-              <Button variant="contained" color="error" onClick={() => handleObrisi(korisnik.id)} style={{ width: "100%", marginTop: "15px" }}>
-                Obriši nalog
-              </Button>
-            </Paper>
-          </>
-        ))}
-      </Paper>
+      {isAdmin ? (
+        <>
+          <h1 style={{ color: "white" }}>Korisnici</h1>
+          <Paper elevation={3} style={paperStyle}>
+            {korisnici.map((korisnik) => (
+              <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left", color: "black" }} key={korisnik.id}>
+                Id: {korisnik.id} <br />
+                Ime: {korisnik.ime} <br />
+                Prezime: {korisnik.prezime} <br />
+                Datum rođenja: {korisnik.datumRodjenja} <br />
+                Email: {korisnik.email} <br />
+                Broj telefona: {korisnik.brojTelefona} <br />
+                Spol: {korisnik.spol}
+                <Button variant="contained" color="error" onClick={() => handleObrisi(korisnik.id)} style={{ width: "100%", marginTop: "15px" }}>
+                  Obriši nalog
+                </Button>
+              </Paper>
+            ))}
+          </Paper>
+        </>
+      ) : (
+        <Paper elevation={3} style={paperStyle}>
+          <p style={{ textAlign: "center", color: "black" }}>Nemate pristup. Kontaktirajte administratora.</p>
+        </Paper>
+      )}
     </Container>
   );
-}
+      }  
